a8664d11daa6ae2cfc1b4fd2a1a5a53c167af02f
Lucas Martins <lucas.martins.c03@gmail.com>
2020-08-26 17:02:19 -0300

    Problems with navigator

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, Navigator, View, TextInput } from "react-native";
+
+import { Link } from "@react-navigation/native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      {/* <Navigator
+        initialRoute={{ title: "Awesome Scene", index: 0 }}
+        renderScene={(route, navigator) => <Text>Hello {route.title}!</Text>}
+      /> */}
+      {/* <View>
+        <Text>Login</Text>
+        <TextInput placeholder="What is your user name?" />
+      </View>
+      <View>
+        <Text>Senha</Text>
+        <TextInput placeholder="Write your password here!" />
+      </View> */}
     </View>
   );
 }
@@ -14,8 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
